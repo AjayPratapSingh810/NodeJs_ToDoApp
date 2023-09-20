ef6478bf9461dc73c2d867eca3d7a6b7084c1788
unknown <pratapaj810@gmail.com>
2023-09-21 00:42:56 +0530

    fixed engine

--- data/database.js
+++ data/database.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
 //------------------ DataBase---------------
-export const connectDB = () =>{
-    mongoose.connect(process.env.MONGO_URI,{
-    dbName: "ToDoApp",
-}).then(()=>{
-    console.log("database connected");
-}).catch((error)=>{
-    console.log("database error");
-})
+export const connectDB = () => {
+    mongoose.connect(process.env.MONGO_URI, {
+        dbName: "ToDoApp",
+    }).then((c) => {
+        console.log(`database connected with ${c.connection.host}`);
+    }).catch((error) => {
+        console.log("database error");
+    })
 };
